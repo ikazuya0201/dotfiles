e49675685fc9e6feeffad5c8a1853c7b198137cd
Kazuya Iimuro <ikazuya0201@gmail.com>
2019-04-18 14:01:27 +0900

    [update] Modified gitignore and link creation script.

--- create_links.sh
+++ create_links.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitignore
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zsh ~/.zsh
