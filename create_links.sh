82a1e3f2fc63fcabb487e24f3395221d279584d8
Kazuya Iimuro <ikazuya0201@gmail.com>
2019-04-20 23:00:28 +0900

    [delete] Removed link for .zsh directory.

--- create_links.sh
+++ create_links.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.zsh ~/.zsh
